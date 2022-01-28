70f20baa42f01cdb0274d650c55050df95714278
Travis <tzito93@gmail.com>
2022-01-28 13:24:09 -0600

    Squares change color when hovered over

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ for (i = 0; i < 16; i++) {
     for (j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.className = "square";
+        square.addEventListener("mouseover", () => {
+                square.classList.add("hovered");
+            }, { once: true});
         row.appendChild(square);
     }
     document.getElementById("container").appendChild(row);
