e64eb3454e594392a09af7aafa0ef474b4080ab5
Travis <tzito93@gmail.com>
2022-01-31 18:53:12 -0600

    Moved grid generation to a function that can take in any grid size

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-for (i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.id = "row";    
-    for (j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        square.addEventListener("mouseover", () => {
-                square.classList.add("hovered");
-            }, { once: true});
-        row.appendChild(square);
+function createGrid(length) {
+    for (i = 0; i < length; i++) {
+        const row = document.createElement("div");
+        row.id = "row";    
+        for (j = 0; j < length; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.addEventListener("mouseover", () => {
+                    square.classList.add("hovered");
+                }, { once: true});
+            row.appendChild(square);
+        }
+        document.getElementById("container").appendChild(row);
     }
-    document.getElementById("container").appendChild(row);
 }
+
+createGrid(16);
